a00fa16f9dca3f12c03b1c60154bf28750c9bb54
Sailesh <126142666+Sailesh3000@users.noreply.github.com>
2024-05-18 18:30:27 +0530

    Add files via upload

---
+++ Login (Frontend)/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from "./components/Login"
+
+function App() {
+  return (
+    <div className='text-center w-96 m-auto pt-60'>
+    <Login />
+    </div>
+  );
+}
+
+export default App;
